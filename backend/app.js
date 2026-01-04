1c4b9bc0c6251e33dfb2a789046efc00700f79a2
Engineer_DUDE <luka.sever20@gmail.com>
2026-01-04 14:14:14 +0100

    Dodan REST API

--- backend/app.js
+++ backend/app.js
@@ -1,22 +1,21 @@
 const express = require("express");
 const cors = require("cors");
-const mongo = require("mongodb");
 const mongoose = require("mongoose");
 
-const app = express()
-
-app.use(cors())
+const app = express();
+app.use(cors());
+app.use(express.json())
 
 const uri = "mongodb+srv://lukasever20_db_user:CmEFXS26TulwSuTC@cluster0.xotdmaq.mongodb.net/satelitiDB?appName=Cluster0";
 
 mongoose.connect(uri).then(res => {
-    console.log("Connected to database");
+  console.log("Connected to database");
 });
 
 const schema = new mongoose.Schema({
-  ime_satelita: { type: String, required: true },
-  drzava_porijekla: { type: String, required: true },
-  drzava_odrzavanja: { type: String, required: true },
+  "ime satelita": { type: String, required: true },
+  "drzava porijekla": { type: String, required: true },
+  "drzava odrzavanja": { type: String, required: true },
   vlasnik: { type: String, required: true },
   korist: { type: String, required: true },
   orbita: { type: String, required: true },
@@ -29,16 +28,241 @@ const schema = new mongoose.Schema({
 
 const Sateliti = mongoose.model("Sateliti", schema, "sateliti");
 
-app.get("/api/data", async (req, res) => {
-    try {
-        const sateliti = await Sateliti.find({}).select("-_id");
-        res.json(sateliti)
-    } catch (err) {
-        console.log(err);
+// pomoćna funkcija vraća objekt filtriran sa stranice
+function buildFilter(customSearchColumn, customSearchValue) {
+  if (!customSearchValue.trim()) return {}; //prazan string nas ne zanima
+
+  const searchValue = String(customSearchValue).trim();
+
+  if (customSearchColumn && customSearchColumn !== "all") {
+    const numericFields = ['inklinacija', 'period', 'perigej', 'apogej']; // brojevi su malo problematični
+    if (numericFields.includes(customSearchColumn)) {
+      const numValue = parseFloat(searchValue);
+      if (!isNaN(numValue)) {
+        return { [customSearchColumn]: numValue };
+      }
+      return {};
     }
-})
+    return { [customSearchColumn]: { $regex: searchValue, $options: "i" } }; 
+        // regex je uvijek samo vrijenost koju tražimo, "i" znači da nas velika i mala slova ne zanimaju.
+
+  }
+
+  // (Svi stupci)
+  const regex = { $regex: searchValue, $options: "i" };
+  return {
+    $or: [
+      { "ime satelita": regex },
+      { "drzava porijekla": regex },
+      { "drzava odrzavanja": regex },
+      { vlasnik: regex },
+      { korist: regex },
+      { svrha: regex },
+      { orbita: regex }
+    ]
+  };
+}
+
+// Glavni endpoint za podatke
+app.get("/search", async (req, res) => {
+  try {
+    // ovi podaci su samo za DataTable, da stranica zna koliko podataka prikazati itd.
+    const draw = parseInt(req.query.draw) || 1;
+    const start = parseInt(req.query.start) || 0;
+    const length = parseInt(req.query.length) || 10;
+    
+    // odabrani stupac
+    const customSearchColumn = req.query.customSearchColumn;
+    const customSearchValue = req.query.customSearchValue || "";
+    
+    const filter = buildFilter(customSearchColumn, customSearchValue);
+    const totalRecords = await Sateliti.countDocuments();
+    const filteredRecords = await Sateliti.countDocuments(filter);
+
+    const data = await Sateliti.find(filter)
+      .skip(start)
+      .limit(length)
+      .lean(); // vrati kao JSON, bez pretvaranja u objekt za MongoDB
+
+    res.json({
+      draw,
+      recordsTotal : totalRecords,
+      recordsFiltered: filteredRecords,
+      data
+    });
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ error: "Internal server error" });
+  }
+});
+
+app.get("/allSatellites", async (req, res) => {
+  const data = await Sateliti.find().lean();
+  res.status(200).send(data);
+});
+
+app.get("/americans", async (req, res) => {
+  const response = await Sateliti.find( {"drzava porijekla" : "SAD"}).lean();
+  res.status(200).send(response);
+});
+
+app.get("/leo", async (req, res) => {
+  const response = await Sateliti.find( {"orbita" : "LEO"} ).lean();
+  res.status(200).send(response);
+});
+
+app.get("/meo", async (req, res) => {
+  const response = await Sateliti.find( {"orbita" : "MEO"} ).lean();
+  res.status(200).send(response);
+});
+
+app.get("/satellites/:sateliteId" ,async (req, res) => {
+  const sateliteId = req.params.sateliteId;
+  try {
+    const response = await Sateliti.findOne({ "_id" : sateliteId}).lean();
+    if (!response) {
+      return res.status(404).json({
+        error: "Satelit nije pronađen",
+        querry: sateliteId
+      });
+    }
+    
+    res.json(response);
+  } catch (e) {
+    res.status(500).json({
+      error: "Greška u bazi"
+    });
+  }
+});
+
+app.post("/satellite", async (req, res) => {
+  try {
+    const {
+      "ime satelita": imeSatelita,
+      "drzava porijekla": drzavaPorijekla,
+      "drzava odrzavanja": drzavaOdrzavanja,
+      vlasnik,
+      korist,
+      orbita,
+      inklinacija,
+      period,
+      svrha,
+      perigej,
+      apogej
+    } = req.body;
+  
+
+  if (!imeSatelita ||
+      !drzavaPorijekla ||
+      !drzavaOdrzavanja ||
+      !vlasnik ||
+      !korist ||
+      !orbita ||
+      inklinacija == null ||
+      period == null ||
+      !svrha ||
+      perigej == null ||
+      apogej == null) {
+        res.status(400).json({error : "Nedostaju podaci o satelitu!"});
+      }
+
+      const noviSatelit = await Sateliti.create({
+        "ime satelita": imeSatelita,
+        "drzava porijekla": drzavaPorijekla,
+        "drzava odrzavanja": drzavaOdrzavanja,
+        vlasnik,
+        korist,
+        orbita,
+        inklinacija,
+        period,
+        svrha,
+        perigej,
+        apogej
+      });
+
+      res.status(201).json(noviSatelit);
+    } catch (err) {
+      res.status(500).json({ error : "Greška tokom spremanaj u bazu"})
+    }
+});
+
+app.put("/satellite", async (req, res) => {
+  try {
+    const {
+      "ime satelita": imeSatelita,
+      "drzava porijekla": drzavaPorijekla,
+      "drzava odrzavanja": drzavaOdrzavanja,
+      vlasnik,
+      korist,
+      orbita,
+      inklinacija,
+      period,
+      svrha,
+      perigej,
+      apogej
+    } = req.body;
+    console.log("Je li se što ispisuje?");
+    const satelitZaPromjenu = {
+      "ime satelita": imeSatelita,
+      "drzava porijekla": drzavaPorijekla,
+      "drzava odrzavanja": drzavaOdrzavanja,
+      "vlasnik" : vlasnik,
+      "korist" : korist,
+      "orbita" : orbita,
+      "inklinacija" : inklinacija,
+      "period" : period,
+      "svrha" : svrha,
+      "perigej" : perigej,
+      "apogej" : apogej
+    }
+    const response = await Sateliti.exists(satelitZaPromjenu); 
+    if (!response)
+      return res.status(404).json( { error: "Ne postoji objekt u bazi!"});
+    await Sateliti.findOneAndUpdate(satelitZaPromjenu);
+    res.status(200).json({ message : "Satelit osvježen!"});
+  } catch (err) {
+    res.status(500).json({ error: "Greška u komunikaciji s bazom"});
+  }
+});
+
+app.delete("/satellite", async (req, res) => {
+  try {
+    const {
+      "ime satelita": imeSatelita,
+      "drzava porijekla": drzavaPorijekla,
+      "drzava odrzavanja": drzavaOdrzavanja,
+      vlasnik,
+      korist,
+      orbita,
+      inklinacija,
+      period,
+      svrha,
+      perigej,
+      apogej
+    } = req.body;
+    const satelitZaBrisanje = {
+      "ime satelita": imeSatelita,
+      "drzava porijekla": drzavaPorijekla,
+      "drzava odrzavanja": drzavaOdrzavanja,
+      "vlasnik" : vlasnik,
+      "korist" : korist,
+      "orbita" : orbita,
+      "inklinacija" : inklinacija,
+      "period" : period,
+      "svrha" : svrha,
+      "perigej" : perigej,
+      "apogej" : apogej
+    }
+    if (!await Sateliti.exists(satelitZaBrisanje))
+      return res.status(500).json( { error: "Ne postoji objekt u bazi!"});
+    await Sateliti.findOneAndDelete(satelitZaBrisanje);
+    res.status(200).json({ message : "Satelit izbrisan!"});
+  } catch (err) {
+    res.status(500).json({ error: "Greška u komunikaciji s bazom"});
+  }
+
+});
 
 app.listen(3000, () => {
-    console.log("Running on port 3000");
-    
+  console.log("Running on port 3000");
 });
--- frontend/src/Datatable.jsx
+++ frontend/src/Datatable.jsx
@@ -1,20 +1,18 @@
-import axios from 'axios';
-import { use, useEffect, useRef, useState } from 'react';
+import { useCallback, useEffect, useRef, useState } from 'react';
 import DataTable from 'datatables.net-react';
 import DT from 'datatables.net-dt';
 import Papa from "papaparse"
-
-import './App.css'
 
 DataTable.use(DT);
 
 export default function Datatable() {
     const table = useRef();
+    const [tableInstance, setTableInstance] = useState(null);
     const downloadLinkJson = useRef();
     const downloadLinkCsv = useRef();
-
-    const [rowData, setRowData] = useState([]);
     const [searchColumn, setSearchColumn] = useState("all");
+    const searchColumnRef = useRef(searchColumn)
+    
     const columns = [
         'Ime satelita',
         'Država porijekla',
@@ -28,41 +26,29 @@ export default function Datatable() {
         'Inklinacija',
         'Period'
     ];
+
     const keys = ["ime satelita", "drzava porijekla", "drzava odrzavanja", "vlasnik", "korist", "svrha", "orbita", "perigej", "apogej", "inklinacija", "period"];
+    
+    // podaci koji će biti prikazani na stranici
+    const dtColumns = keys.map(k => ({data: k, title: columns[keys.indexOf(k)]}));
+    const searchValueRef = useRef("");
 
-    // ovaj useEffect dobiva podatke iz baze
     useEffect(() => {
-        axios.get("http://localhost:3000/api/data").then((res) => {
-            const data = res.data; // podaci iz odgovora
-
-            // izaberi koje podatke koje želimo prikazati (ne zanima nas _id)
-            const transformedRows = data.map(obj => keys.map(key => obj[key]));
-            setRowData(transformedRows);
-        });
-        1
-    }, []);
+        searchColumnRef.current = searchColumn;
+    }, [searchColumn])
 
     useEffect(() => {
         if (!table.current) return;
         const dt = table.current.dt();
+        setTableInstance(dt);
 
         const input = document.querySelector('#dt-search-1');
 
         if (!input) return;
 
         const handleSearchInput = (e) => {
-            const val = e.target.value;
-
-            if (searchColumn === 'all') {
-                dt.columns().search('');
-                dt.search(val).draw();
-            } else {
-                const colIndex = keys.indexOf(searchColumn);
-                if (colIndex === -1) return;
-
-                dt.search('').columns().search('');
-                dt.column(colIndex).search(val).draw();
-            }
+            searchValueRef.current = e.target.value;
+            dt.ajax.reload(); // moramo zvat isvaki puta kada se promijeni pretraživanje
         };
 
         input.addEventListener('input', handleSearchInput);
@@ -70,24 +56,38 @@ export default function Datatable() {
         return () => {
             input.removeEventListener('input', handleSearchInput);
         };
-    }, [searchColumn, keys]);
+    }, []);
+
+    const handleDropdownChange = useCallback((e) => {
+        setSearchColumn(e.target.value);
+        if (tableInstance)
+            tableInstance.ajax.reload();
+    }, [tableInstance]);
+
+    const ajaxOption = useCallback(() => ({
+        url: "http://localhost:3000/search",
+        type: "GET",
+        data: function (d) {
+            d.customSearchColumn = searchColumnRef.current;
+            d.customSearchValue = searchValueRef.current;
+        }
+    }), []);
+
 
     // ovaj useEffect stvara linkove za preuzimanje
     useEffect(() => {
         if (!table.current) return;
         const dt = table.current.dt();
 
-        const getFilteredData = () => dt.rows({ search: "applied" }).data().toArray();
-
         const updateDownloadLinks = () => {
-            const filteredData = getFilteredData();
+            const filteredData = dt.rows({ search: "applied" }).data().toArray();
 
             // pretvori array od arraya u array s objektima
             const transform = (data) =>
                 data.map(row => {
                     const obj = {};
-                    keys.forEach((key, i) => {
-                        obj[key] = row[i];
+                    keys.forEach((key) => {
+                        obj[key] = row[key];
                     });
                     return obj;
                 });
@@ -107,38 +107,49 @@ export default function Datatable() {
             downloadLinkJson.current.href = URL.createObjectURL(jsonBlob);
             downloadLinkJson.current.download = "filteredData.json";
 
-            const csvData = [columns, ...filteredData];
+            const csvRows = filteredData.map(row => keys.map(k => row[k]));
+            const csvData = [columns, ...csvRows];
             const csvString = Papa.unparse(csvData);
             const csvBlob = new Blob([csvString], { type: "text/csv" });
             downloadLinkCsv.current.href = URL.createObjectURL(csvBlob);
             downloadLinkCsv.current.download = "filteredData.csv";
         };
 
-        dt.on("search.dt", updateDownloadLinks);
-
-        updateDownloadLinks();
-
+        dt.on("draw", updateDownloadLinks);
         return () => {
-            dt.off("search.dt", updateDownloadLinks);
+            dt.off("draw", updateDownloadLinks);
             if (downloadLinkJson.current?.href) URL.revokeObjectURL(downloadLinkJson.current.href);
             if (downloadLinkCsv.current?.href) URL.revokeObjectURL(downloadLinkCsv.current.href);
         };
-    }, [rowData, columns, keys]);
+    }, [columns, keys]);
 
 
     return (
         <>
             <br />
             <label htmlFor='filter'>Traži po atributu: </label>
-            <select id="filter" value={searchColumn}
-                onChange={e => setSearchColumn(e.target.value)}
+            <select id="filter" 
+                    value={searchColumn}
+                    onChange={handleDropdownChange}
             >
                 <option value="all">(Svi stupci)</option>
                 {keys.map((key, index) => (
                     <option key={index} value={key}>{columns[index]}</option>
                 ))}
             </select>
-            <DataTable data={rowData} ref={table}>
+            <DataTable 
+                ref={table}
+                ajax={ajaxOption()}
+                columns={dtColumns}
+                options={{
+                    processing: true,
+                    serverSide: true,
+                    searching: true,
+                    columnDefs: [
+                        {searchable: false, targets: "_all"}
+                        ]
+                    }
+                }>
                 <thead>
                     <tr>
                         {columns.map((col, i) => <th key={i}>{col}</th>)}
